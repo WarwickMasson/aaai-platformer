09154b65738f03e99138279a6045978c1433a5b9
Warwick Masson <warwickmasson@gmail.com>
2015-07-27 10:17:24 +0200

    Increase enemy speed, add noise to movement, increase width and gap.

--- simulator.py
+++ simulator.py
@@ -20,11 +20,11 @@ def bound_vector(vect, xmax, ymax):
     yval = bound(vect[1], -ymax, ymax)
     return vector(xval, yval)
 
-WIDTH1 = 300
-WIDTH2 = 300
-WIDTH3 = 300
-GAP1 = 50
-GAP2 = 100
+WIDTH1 = 250
+WIDTH2 = 320
+WIDTH3 = 100
+GAP1 = 75
+GAP2 = 125
 MAX_PLATWIDTH = max([WIDTH1, WIDTH2, WIDTH3])
 PLATHEIGHT = 40.0
 HEIGHT_DIFF = 50.0 - PLATHEIGHT
@@ -34,9 +34,10 @@ MAX_DX = 50.0
 MAX_DY = 50.0
 MAX_DDX = 20.0 / DT
 MAX_DDY = MAX_DY / DT
-ENEMY_SPEED = 20.0
+ENEMY_SPEED = 30.0
 LEAP_DEV = 200.0
 HOP_DEV = 20.0
+ENEMY_NOISE = 5.0
 
 class Platform:
     ''' Represents a fixed platform. '''
@@ -183,6 +184,8 @@ class Enemy:
         right = self.platform.position[0] + self.platform.size[0] - self.size[0]
         if not self.platform.position[0] < self.position[0] < right:
             self.dx *= -1
+        self.dx += np.random.normal(0.0, ENEMY_NOISE*dt)
+        self.dx = bound(self.dx, -ENEMY_SPEED, ENEMY_SPEED)
         self.position[0] += self.dx * dt
         self.position[0] = bound(self.position[0], self.platform.position[0], right)
 
@@ -206,6 +209,7 @@ class Player(Enemy):
         ''' Applies a power to the entity in direction theta. '''
         accel = bound_vector(accel, MAX_DDX, MAX_DDY)
         self.velocity += accel * dt
+        self.velocity[0] += np.random.normal(0.0, ENEMY_NOISE*dt)
         self.velocity = bound_vector(self.velocity, MAX_DX, MAX_DY)
 
     def run(self, power, dt):
